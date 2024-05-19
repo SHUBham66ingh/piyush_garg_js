f06ab5da989c3801be5debdf38fe15cba2faa695
Shubham singh <shubham1041singh@gmail.com>
2024-05-19 23:44:45 +0530

    ..

---
+++ variable.js
@@ -0,0 +1,21 @@
+// console.log("shubham");
+// js is loosely typed language...
+// var scope is global...
+// var age=22
+// var num1='piysuh'
+// var num2=23
+
+// console.log(age + num1)
+// //naming firstName
+// if(true)
+//     {
+//         var score=100;
+//     }
+// console.log(score)
+// let scope is local;
+let age=21
+age=22
+console.log(age)
+
+
+
