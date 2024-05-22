c20b70e47c1db5be559e9d22208518c652ce9030
Shubham singh <shubham1041singh@gmail.com>
2024-05-22 17:59:01 +0530

    ..

--- conditional.js
+++ conditional.js
@@ -1,12 +1,27 @@
-const age=30;
+// const age=30;
+
+// if( age>= 18)
+//     {
+//    console.log("eligible for voting");
+//     }
+//     else{
+//    console.log("you cannot vote");
+//     }
 
 
-if( age>= 18)
-    {
-   console.log("eligible for voting");
-    }
-    else{
-   console.log("you cannot vote");
-    }
+// // ternary operators
 
-    // ternary operators
+// const age=20
+// age>=18 ? console.log("yes") : console.log("no")
+
+const option =1;
+
+switch(option)
+{
+     case 1:{
+        console.log("namaste");
+     }
+     case 2:{
+        console.log("hello");
+     }
+}
