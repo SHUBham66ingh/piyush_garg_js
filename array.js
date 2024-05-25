f081ecb5576a8ed2a897f312d56e54ac20f7f9aa
Shubham singh <shubham1041singh@gmail.com>
2024-05-25 21:53:11 +0530

    ......

--- array.js
+++ array.js
@@ -1,12 +1,16 @@
 // arrays
 const students=['aman'  , 'john'  , 'virat' , 'bill'];
-const marks=[1,2,3,4,5];
+// const marks=[1,2,3,4,5];
 
-const myArray =[ 1, true , 'shubham'];
-students[0]="shubham"
-console.log(students[0]);
+// const myArray =[ 1, true , 'shubham'];
+// students[0]="shubham"
+// console.log(students[0]);
 
 
-students.push("doku");
-students.push("siri");
+
+// students.push("doku");
+// students.push("siri");
+// console.log(students);
+students.pop();
+console.log(students.indexOf("bill"))
 console.log(students);
