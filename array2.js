40bd1f395b2df722c0a571d5ac9ca2523d5a1239
Shubham singh <shubham1041singh@gmail.com>
2024-05-25 22:47:26 +0530

    ...

--- array2.js
+++ array2.js
@@ -1,3 +1,16 @@
 const numbers = [1,2,3,4,5,6,7,8,9]
+// function double(n)
+// {
+//     return n*2;
+// }
 
-numbers.map((val) => console.log(val*2))
+// let newArr = numbers.map(double)
+
+// console.log(newArr);
+
+// let ans=numbers.find(num=> num==4)
+
+// console.log(ans);
+const newArr = numbers.filter(num=>num % 2==0)
+
+console.log(newArr)
