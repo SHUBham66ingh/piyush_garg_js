c910cd0e6795324e710d2df9dad09a93fafa873d
Shubham singh <shubham1041singh@gmail.com>
2024-05-25 21:16:55 +0530

    ...

--- array.js
+++ array.js
@@ -1,3 +1,4 @@
 // arrays
-const students=['aman'  , 'john'  , 'virat'];
-console.log(students);
+const students=['aman'  , 'john'  , 'virat' , 'bill'];
+students[0]="shubham"
+console.log(students[0]);
