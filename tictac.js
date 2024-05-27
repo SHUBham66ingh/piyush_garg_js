00882243e170b28347afcdf75a9ed668ec0a0671
Shubham singh <shubham1041singh@gmail.com>
2024-05-27 13:02:22 +0530

    ....

--- tictac.js
+++ tictac.js
@@ -1,10 +1,29 @@
 
 let currentPlayer="X";
-let array=Array(9).fill(null);
+let arr=Array(9).fill(null);
 
-console.log(array)
+
+function checkWinner(){
+if(
+  ( arr[0]!==null   && arr[0]==arr[1]  && arr[1]==arr[2]) ||
+  (  arr[0]!==null  && arr[3]==arr[4]  && arr[4]==arr[5]) ||
+  (  arr[0]!==null  && arr[6]==arr[7]  && arr[7]==arr[8]) ||
+  (  arr[0]!==null  && arr[0]==arr[3]  && arr[3]==arr[6]) ||
+  (  arr[0]!==null  && arr[1]==arr[4]  && arr[4]==arr[7]) ||
+  (  arr[0]!==null  && arr[2]==arr[5]  && arr[5]==arr[8]) ||
+  (  arr[0]!==null  && arr[0]==arr[4]  && arr[4]==arr[8]) ||
+  (  arr[0]!==null  && arr[2]==arr[4]  && arr[4]==arr[6]) 
+)
+
+}
+
+
 function handleClick(el)
 {
-    const id= el.id;
-    console.log(id);
+    const id= Number(el.id);
+    arr[id] = currentPlayer;
+    el.innerText = currentPlayer;
+    checkWinner();
+    currentPlayer = currentPlayer==='X' ? '0' : 'X';
+  
 }
